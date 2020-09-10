f2d3e73a5abdec7b1151f5a13094f2d400ceee05
Sanghoon Lim <shlim1986@gmail.com>
2020-09-10 12:55:24 +0900

    update

--- MultDep_pp13TeV/DrawEfficiency.C
+++ MultDep_pp13TeV/DrawEfficiency.C
@@ -1,7 +1,7 @@
 void DrawEfficiency(){
 
 	const int nset = 3;
-	const char *setname[nset] = {"LHC16MC", "LHC17MC", "LHC18MC"};
+	string setname[nset] = {"LHC16MC", "LHC17MC", "LHC18MC"};
 	const int nColor[nset] = {1, 2, 4};
 
 	TFile *infile = new TFile("outfile_efficiency.root","read");
@@ -27,6 +27,28 @@ void DrawEfficiency(){
 			h1dReco_SPDV_com->Add(h1dReco_SPDV[iset]);
 		}
 
+	}
+
+	/*
+	TCanvas *c1 = new TCanvas("c1","c1",2*1.1*400,2*400);
+	c1->Divide(2,2);
+
+	for (int iset=0; iset<nset; iset++){
+		c1->cd(iset+1);
+		h1dEff_SPDV[iset]->Draw("p");
+	}
+	*/
+
+	TH1D *h1dEff_SPDV_com = (TH1D*)h1dReco_SPDV_com->Clone("h1dEff_SPDV_com");
+	h1dEff_SPDV_com->Sumw2();
+	h1dEff_SPDV_com->Divide(h1dGen_SPDV_com);
+
+	TCanvas *c1 = new TCanvas("c1","c1",1.1*400,400);
+	{
+		TH1D *htmp = (TH1D*)gPad->DrawFrame(0,0,100,0.15);
+		htmp->GetXaxis()->SetTitle("Number of SPD tracklets");
+		htmp->GetYaxis()->SetTitle("Efficiency");
+		h1dEff_SPDV_com->Draw("p same");
 	}
 
 
